0a5b15896c8a8b09abf9dd4ec7b63d51c90f350b
pxbtf <pxiou1997@163.com>
2024-04-05 19:37:00 +0800

    refactor(plugin): :recycle: 优化代码结构

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "vue-eslint-parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-essential",
+    "./.eslintrc-auto-import.json",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "parser":"vue-eslint-parser",
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential",
-        "./.eslintrc-auto-import.json"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    "rules": {
-        // 关闭名称校验
-        'vue/multi-word-component-names': "off" 
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue"],
+  rules: {
+    // 关闭名称校验
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import router from "@/router";
+import { setupStore } from "@/store";
+import { setupElIcons, setupI18n } from "@/plugins";
+import "./style.css";
 import "virtual:svg-icons-register";
 import "uno.css";
-import router from "@/router";
-const app = createApp(App);
-import i18n from "@/lang/index";
 // 引入暗黑模式
 import "element-plus/theme-chalk/dark/css-vars.css";
+
+const app = createApp(App);
+// 全局注册store
+setupStore(app);
+// 全局注册图标
+setupElIcons(app);
+// 国际化
+setupI18n(app);
 app.use(router);
-app.use(i18n);
 app.mount("#app");
